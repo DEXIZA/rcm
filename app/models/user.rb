ca258885522293a4e2d6cd1064279f48a88cee8f
DEXIZA <dexizaia@gmail.com>
2021-09-11 18:13:34 +0900

    バリデーションの設定、deviceの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+  validates :name
+  validates :birthday
+  validates :phone
+    with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+      validates :kana
+    end
+  end
 end
