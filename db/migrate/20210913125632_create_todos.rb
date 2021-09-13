a8a7c4efdf150db4d215ca2da9748d93e09235bc
DEXIZA <dexizaia@gmail.com>
2021-09-13 22:01:42 +0900

    todoテーブル　カラム追加、migration

--- db/migrate/20210913125632_create_todos.rb
+++ db/migrate/20210913125632_create_todos.rb
@@ -1,7 +1,11 @@
 class CreateTodos < ActiveRecord::Migration[6.0]
   def change
     create_table :todos do |t|
-
+      t.string :title,  null: false
+      t.text :content,  null: false
+      t.integer :urgency,  null: false
+      t.integer :who,  null: false
+      t.references :user,  null: false, foreign_key: true
       t.timestamps
     end
   end
