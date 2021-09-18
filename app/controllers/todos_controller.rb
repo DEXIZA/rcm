7391a154ab145ad10d84fd10a639b423c2c34741
DEXIZA <dexizaia@gmail.com>
2021-09-18 12:29:00 +0900

    編集機能、削除機能の実装

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,8 @@
 class TodosController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  before_action :set_todo, only: [:show]
+  before_action :set_todo, only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+
 
 
 
@@ -24,6 +26,25 @@ class TodosController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to todo_path(@todo.id)
+    else
+      render :edit
+    end
+    # 更新成功→詳細画面偏移　更新失敗→編集画面偏移の処理
+  end
+
+
+
+  def destroy
+    @todo.destroy
+    redirect_to root_path
+  end
+
   private
 
   def todo_params
@@ -39,6 +60,10 @@ class TodosController < ApplicationController
 
   def set_todo
     @todo = Todo.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless current_user.id == @todo.user_id
   end
 
 end
